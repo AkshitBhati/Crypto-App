861645d032c73cb4ebee1e67290f67eb7a3ad0c1
AkshitBhati <akshitworks24@gmail.com>
2023-08-13 13:57:06 +0530

    update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import TrendingCoins from './TrendingCoins'
+import { BsSearch } from "react-icons/bs"
 
 const Home = () => {
   return (
     <>
-    <div>
-      <h2 className='text-white'>Trending Coins</h2>
-        CoinsList
+    <div className='flex home__search'>
+      <input type="text" />
+      <button className='border bg-white text-xl'><BsSearch /></button>
+    </div>
+    <div className='p-5 mt-3 font-semibold'>
+      <h2 className='text-white text-2xl'>Trending Coins</h2>
+        <TrendingCoins />
     </div>
     </>
   )
