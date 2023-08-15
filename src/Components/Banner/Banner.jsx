5d65c9b8022e9906b824eb744ad80d47adc141c6
AkshitBhati <akshitworks24@gmail.com>
2023-08-15 20:12:54 +0530

    updated banner name

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,9 +3,15 @@ import "./Banner.css"
 
 const Banner = () => {
   return (
+    <>
     <div className='banner'>
         <img src="https://img.freepik.com/free-vector/gradient-style-technology-background_23-2148884154.jpg" alt="bannerImage" className='banner__img'/>
     </div>
+
+    <div className='banner__name'>
+    <h1>Crypto App</h1>
+    </div>
+    </>
   )
 }
 
