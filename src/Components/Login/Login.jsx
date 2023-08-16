f67848b5cc04c3bea1cbeda5ee634fdc04756688
AkshitBhati <akshitworks24@gmail.com>
2023-08-16 14:16:46 +0530

    login updating

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import "./Login.css"
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className='login'>
+        <h1 className='login__heading'>Login</h1>
+        <form className='login__form'>
+            <label >Name</label>
+            <input type="text" placeholder='Enter Name'/>
+            <label >Email</label>
+            <input type="email" placeholder='Enter Email'/>
+            <label >Password</label>
+            <input type="password" placeholder='Enter Password'/>
+
+        </form>
+        <div className='login__btn--container'>
+        <button className='login__btn'>Submit</button>
+        </div>
     </div>
   )
 }
