6b47e0e7608a5aaaa7ca20713a9675fcc29a5714
AkshitBhati <akshitworks24@gmail.com>
2023-08-18 11:50:43 +0530

    fetched trending coins

--- src/Components/Trending Coins/TrendingCoins.jsx
+++ src/Components/Trending Coins/TrendingCoins.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
 import "./TrendingCoins.css"
 
 const TrendingCoins = () => {
+    const [coinsData, setCoinsData] = useState([])
+
+    //FUNCTION FOR FETCHING TRENDING COINS
+    const fetchTendingCoins = async() => {
+        const trendingCoins =  await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h")
+        const trendingCoinsData = trendingCoins.data
+        console.log(trendingCoinsData)
+        setCoinsData(trendingCoinsData)
+    }
+
+    useEffect(() => {
+        fetchTendingCoins()
+    }, [])
   return (
-    <div>
-      
+    <div className='trendingCoins'>
+        {
+            coinsData.map((coins) => (
+                <div key={coins.id} className='trendingCoins__wrapper'>
+                    <img src={coins.image} alt="coins-image" className='trendingCoins__img' />
+                    <h4>{coins.name}</h4>
+                </div>
+            ))
+        }
     </div>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../Components/Banner/Banner'
+import TrendingCoins from '../Components/Trending Coins/TrendingCoins'
 
 const Home = () => {
   return (
     <div>
      <Banner /> 
+     <TrendingCoins />
     </div>
   )
 }
